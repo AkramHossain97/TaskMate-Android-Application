067d750c79ecca0d36441fcd515c91e941db4c57
Akram Hossain <akram.hossain03@northsouth.edu>
2021-07-04 22:37:44 +0600

    Splash Screen Added

--- TaskMate/app/src/main/java/com/example/taskmate/MainActivity.java
+++ TaskMate/app/src/main/java/com/example/taskmate/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.taskmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    LottieAnimationView SplashView;
+    LottieAnimationView SplashScreenView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white)); //status bar or the time bar at the top
+        }
+
+        SplashScreenView = findViewById(R.id.splashScreenVariable);
+
     }
 }
